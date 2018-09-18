32c043f8968bff851a7f8bec16cfc01f53494306
Peter Neal <peterneal150@gmail.com>
2018-09-18 18:52:41 -0500

    can now register a user

--- FitnessApp/FitnessApp/auth.py
+++ FitnessApp/FitnessApp/auth.py
@@ -6,3 +6,25 @@ from FitnessApp.db import get_db
 
 auth = Blueprint('auth', __name__, url_prefix='/auth') #create a new blueprint named auth, append "/auth" to associated urls
 
+@auth.route('/register', methods=('GET', 'POST'))
+def register():
+	if request.method == 'POST':
+		#A user wants to register!
+		username = request.form['username'] #get the username and password from the form post
+		password = request.form['password']	
+		db = get_db() #get access to the sql db
+		error = None
+
+		if not username:
+			error = "Username is required!"
+		elif not password:
+			error = "Password is required!"
+		elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
+			error = "User is already registered!"
+
+		if error is None: #make that new user yeh boit
+			db.execute('INSERT INTO user (username, password) VALUES (?,?)', (username, generate_password_hash(password)))
+			db.commit()
+			return "success" #plain text kicks ass. indicates a user was successfully registered.
+
+	return render_template('auth/register.html')
--- FitnessApp/FitnessApp/views.py
+++ FitnessApp/FitnessApp/views.py
@@ -36,11 +36,3 @@ def about():
         year=datetime.now().year,
         message='Hey, I went ahead and set up a Visual Studio solution for us with Flask. Check out the templates, how they work with the CSS, and how the views work. Feel free to shoot me a text or a Teams with questions or anything, if anyone has any treouble.'
     )
-
-@app.route('/register', methods=('GET', 'POST'))
-def register():
-	if request.method == 'POST':
-		#It's a new user!
-		#probably handle that here?
-		print("new user!")
-	return render_template('auth/register.html')
