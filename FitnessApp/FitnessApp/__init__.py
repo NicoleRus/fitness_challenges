8f41e871e2f32da84b2705208d03f55197569065
Peter Neal <peterneal150@gmail.com>
2018-09-17 22:50:46 -0500

    sqlite initialization

--- FitnessApp/FitnessApp/__init__.py
+++ FitnessApp/FitnessApp/__init__.py
@@ -1,8 +1,22 @@
 """
 The flask application package.
 """
-
+import os
+from . import db
 from flask import Flask
-app = Flask(__name__)
+def create_app():
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(DATABASE=os.path.join(app.instance_path, "FitnessApp.sqlite"),)
+	db.init_app(app)
+
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+
+	return app
+
+app = create_app()
 
 import FitnessApp.views
--- FitnessApp/runserver.py
+++ FitnessApp/runserver.py
@@ -4,6 +4,7 @@ This script runs the FitnessApp application using a development server.
 
 from os import environ
 from FitnessApp import app
+from FitnessApp import db
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -11,4 +12,5 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+
     app.run(HOST, PORT)
