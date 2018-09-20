d68dfceb0045753585da122b94e776e819ffd200
Peter Neal <peterneal150@gmail.com>
2018-09-20 12:50:49 -0500

    Login system!

--- FitnessApp/FitnessApp/__init__.py
+++ FitnessApp/FitnessApp/__init__.py
@@ -6,7 +6,7 @@ from . import db
 from flask import Flask
 def create_app():
 	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_mapping(DATABASE=os.path.join(app.instance_path, "FitnessApp.sqlite"),)
+	app.config.from_mapping(DATABASE=os.path.join(app.instance_path, "FitnessApp.sqlite"),SECRET_KEY="password",)
 	db.init_app(app)
 
 	try:
--- FitnessApp/FitnessApp/auth.py
+++ FitnessApp/FitnessApp/auth.py
@@ -28,3 +28,33 @@ def register():
 			return "success" #plain text kicks ass. indicates a user was successfully registered.
 
 	return render_template('auth/register.html')
+
+@auth.route('/login', methods=('GET', 'POST'))
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		db = get_db()
+		error = None
+
+		user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
+		if user is None:
+			error = "Incorrect Username or Password!"
+		elif not check_password_hash(user['password'], password):
+			error = "Incorrect Username or Password!"
+
+		if error == None:
+			session.clear()
+			session['user_id'] = user['id']
+			return redirect(url_for('home'))
+
+	return render_template('auth/login.html')
+
+
+@auth.before_app_request
+def load_logged_in_user():
+	user_id = session.get('user_id')
+	if user_id is None:
+		g.user = None
+	else:
+		g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
